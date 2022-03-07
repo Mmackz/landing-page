541b5c12b68fb3424740cd7524606b3e67bb31bb
Mmackz <matthewmackenz@gmail.com>
2022-03-06 16:10:50 -0800

    🎨 animate dropdown

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const dropdownIcon = document.getElementById("dropdown-icon");
 const links = document.querySelector(".links");
 
-dropdownIcon.addEventListener("click", () => {
+dropdownIcon.addEventListener("click", (event) => {
+   event.stopPropagation();
    const style = window.getComputedStyle(links);
    if (style.display === "none") {
       links.style.display = "flex";
       setTimeout(() => {
          links.style.maxHeight = "300px";
-         links.style.color = "#1f2937"
       }, 1);
    } else {
-      links.style.maxHeight = "0";
-      setTimeout(() => {
-         links.style.display = "none";
-      }, 750);
+      closeDropdown();
    }
+});
+
+links.addEventListener("click", (event) => {
+   event.stopPropagation();
 });
 
 window.addEventListener("resize", () => {
    if (window.innerWidth > 700) {
       if (links.style.display === "none") {
-         links.style = ""
+         links.style = "";
       }
    }
-})
+});
+
+document.body.addEventListener("click", (event) => {
+   const style = window.getComputedStyle(links);
+   if (window.innerWidth <= 700 && style.display === "flex") {
+      closeDropdown();
+   }
+});
+
+function closeDropdown() {
+   links.style.maxHeight = "0";
+   setTimeout(() => {
+      links.style = "";
+   }, 750);
+}
